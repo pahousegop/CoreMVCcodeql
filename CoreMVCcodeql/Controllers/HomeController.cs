9ca14d5dc19b3115ae3763ac4fa4f1f16e72b380
Kyle Wise <kwise@pahousegop.com>
2024-04-25 10:29:30 -0400

    Add project files.

---
+++ CoreMVCcodeql/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using CoreMVCcodeql.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreMVCcodeql.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string UpdateCustomerPassword(string txtUsername, string txtPassword)
+        {
+            string userName = txtUsername;
+            string password = txtPassword;
+
+            Regex testPassword = new Regex(userName);
+            Match match = testPassword.Match(password);
+            if (match.Success)
+            {
+                return "Do not include name in password.";
+            }
+            else
+            {
+                return "Good password.";
+            }
+        }
+    }
+}
---
+++ CoreMVCcodeql/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreMVCcodeql.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
